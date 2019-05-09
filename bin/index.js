00670220fb38094cb729a7d96070caa9e7525cff
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-05-09 09:42:48 +0800

    Implement health check route test

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,49 @@
-const config = require('./../config/index')
 const app = require('./../app')
+const config = require('./../config/index')
+const http = require('http')
+const loggingService = require('./../services/loggingService')
 
-const port = config.get('app.port', 7002)
+const NODE_ENV = config.env('NODE_ENV', 'development')
+let PORT;
 
-app.listen(port, function (err) {
-  if (err) console.error(err)
-  console.log(`listening on port ${port}!`)
-})
+if (NODE_ENV === 'testing') {
+  PORT = 7999
+} else {
+  PORT = config.get('app.port', 7002)
+}
+
+// const port = config.get('app.port', 7002)
+// const server = app.listen(port, function (err) {
+//   if (err) console.error(err)
+//   console.log(`listening on port ${port}!`)
+// })
+// module.exports = server;
+
+const server = http.createServer(app);
+server.listen(PORT);
+
+server.on('listening', function () {
+  loggingService.info(`Listening on Port: ${PORT}`);
+});
+
+server.on('error', function (err) {
+  loggingService.error(err.stack);
+  process.exit(1);
+});
+
+process.once('SIGTERM', function () {
+  loggingService.info("SIGTERM received. Terminating");
+  server.close(function () {
+    process.exit(0);
+  });
+});
+
+process.on('uncaughtException', function (err) {
+  loggingService.error(err.stack);
+});
+
+process.on('unhandledRejection', function (err) {
+  loggingService.error(err.stack);
+});
+
+module.exports = server;
---
+++ test/integration/routes/home.test.js
@@ -0,0 +1,23 @@
+process.env.NODE_ENV = 'testing'
+
+let server = require('./../../../bin/index');
+const httpStatus = require('http-status-codes');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+const route = '/';
+
+describe('health check route /', () => {
+  it('should 200', function (done) {
+    chai.request(server)
+    .get(route)
+    .end((err, res) => {
+      expect(res.status).to.equal(httpStatus.OK);
+      expect(res.body.data).to.equal('health check ok')
+    });
+    done();
+  });
+});
