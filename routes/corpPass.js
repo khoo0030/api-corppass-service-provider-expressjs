8a59a9c20739d8500d4653b4cfe7e54f48d45082
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-05-10 15:56:36 +0800

    Work on CorpPass routes

--- routes/corpPass.js
+++ routes/corpPass.js
@@ -1,5 +1,7 @@
-const config = require('./../config/index')
 const { stringify } = require('flatted')
+const _ = require('lodash')
+const attributeUtil = require('./../util/attributeUtil')
+const config = require('./../config/index')
 const express = require('express')
 const loggingService = require('./../services/loggingService')
 const router = express.Router()
@@ -9,7 +11,7 @@ router.get('/corppass-login', function (req, res) {
   const targetURL = config.env('TARGET_URL', config.get('corpPass.targetURL'))
   const esrvcID = config.env('ESRVC_ID', config.get('corpPass.esrvcID'))
   const redirectURL = spcpClientService.createRedirectURL(targetURL, esrvcID)
-  res.redirect(redirectURL)
+  return res.redirect(redirectURL)
 })
 
 router.get('/assert-saml', (req, res) => {
@@ -21,10 +23,19 @@ router.get('/assert-saml', (req, res) => {
     if (err) {
       loggingService.error(stringify(err))
     } else {
+      // OK. No errors resolving saml assertion.
+      // MockPass returns a base64 encoded attributes object. Use the helpful attributeUtil to get the userInfo object
+      // and authAccess object. Refer to test case to see what the 2 objects look like.
+      const userInfo = attributeUtil.getUserInfo(attributes)
+      // const authAccess = attributeUtil.getAuthAccess(attributes)
 
-      res.cookie('GERALD_KHOO_COOKIE', attributes)
+      // You can do whatever you want with the userInfo and authAccess
+
+      // Then redirect back to back and return any data as cookie
+      res.cookie('CPUID', _.get(userInfo, 'UserInfo.CPUID._text'))
+      res.cookie('CPEntID', _.get(userInfo, 'UserInfo.CPEntID._text'))
     }
-    res.redirect(relayState)
+    return res.redirect(relayState)
   })
 })
 
