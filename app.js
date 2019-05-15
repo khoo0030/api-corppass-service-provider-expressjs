078e98e9714f30b3157a7b6410da96e31d5320e0
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-05-15 21:14:49 +0800

    Refactor middleware usage in app.js

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
 const cors = require('cors')
 const express = require('express')
-const httpStatus = require('http-status-codes')
-const loggingService = require('./services/loggingService')
 const routes = require('./routes')
+const routeLoggingMiddleware = require('./middleware/routeLoggingMiddleware')
+const routeNotFoundMiddleware = require('./middleware/routeNotFoundMiddleware')
+const internalServerErrorMiddleware = require('./middleware/internalServerErrorMiddleware')
 
 const app = express()
 
 app.use(cors())
-
-app.use(function (req, res, next) {
-  loggingService.debug(req.url)
-  next()
-})
-
+app.use(routeLoggingMiddleware)
 app.use(routes)
-
-app.use(function (req, res, next) {
-  return next(res.status(httpStatus.NOT_FOUND).json('Route not found'))
-})
-
-app.use(function (err, req, res) {
-  loggingService.error(err.stack)
-  res.status(err.status || 500)
-  return res.jsonp({
-    status: err.status || 500,
-    message: err.message,
-  })
-})
+app.use(routeNotFoundMiddleware)
+app.use(internalServerErrorMiddleware)
 
 module.exports = app
