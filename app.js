9eec5d8f98a0f2a5fb9493c63a377c5b1b8aec97
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-06-13 15:27:03 +0800

    Modify logging mechanism

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const cors = require('cors')
 const express = require('express')
 const helmet = require('helmet')
+const morgan = require('morgan')
 const routes = require('./routes')
-const routeLoggingMiddleware = require('./middleware/routeLoggingMiddleware')
+const loggingService = require('./services/loggingService');
 const routeNotFoundMiddleware = require('./middleware/routeNotFoundMiddleware')
 const internalServerErrorMiddleware = require('./middleware/internalServerErrorMiddleware')
 
@@ -10,7 +11,7 @@ const app = express()
 
 app.use(helmet())
 app.use(cors())
-app.use(routeLoggingMiddleware)
+app.use(morgan('combined', { stream: loggingService.stream }));
 app.use(routes)
 app.use(routeNotFoundMiddleware)
 app.use(internalServerErrorMiddleware)
--- services/loggingService.js
+++ services/loggingService.js
@@ -2,33 +2,49 @@ const config = require('./../config/index')
 const path = require('path')
 const winston = require('winston')
 
-const NODE_ENV = config.env('NODE_ENV', config.get('app.env', 'development'))
+const NODE_ENV = config.env('NODE_ENV', config.get('app.env'))
 const LOGGING_LEVEL = config.env('LOGGING_LEVEL', config.get('app.logging.level', NODE_ENV === 'production' ? 'debug' : 'info'))
 
 const format = winston.format
 
+const options = {
+  file: {
+    level: LOGGING_LEVEL,
+    format: format.combine(
+      format.metadata({fillExcept: ['level', 'label', 'timestamp', 'message']}),
+      format.json()
+    ),
+    filename: path.resolve(process.cwd(), 'logs', 'winston.log'),
+    handleExceptions: true,
+    json: true,
+    colorize: false,
+  },
+  console: {
+    level: LOGGING_LEVEL,
+    format: format.combine(
+      format.simple(),
+    ),
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
 const loggingService = winston.createLogger({
-  level: LOGGING_LEVEL,
   format: format.combine(
-    format.label({ label: path.basename(process.mainModule.filename) }),
     format.timestamp(),
   ),
   transports: [
-    new winston.transports.Console({
-      format: format.combine(
-        format.colorize(),
-        format.printf(info => `${info.level}: ${info.timestamp} [${info.label}] ${info.message}`),
-      )
-    }),
-    new winston.transports.File({
-      filename: path.resolve(process.cwd(), 'logs', 'winston.log'),
-      format: format.combine(
-        format.metadata({fillExcept: ['level', 'label', 'timestamp', 'message']}),
-        format.json()
-      )
-    })
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
   ],
-  exitOnError: false
-})
+  exitOnError: false,
+});
+
+loggingService.stream = {
+  write: function(message, encoding) {
+    loggingService.info(message);
+  },
+};
 
 module.exports = loggingService
